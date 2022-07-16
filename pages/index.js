478cd6c5d8e09f537a5d9af910151d4a3692154f
Sergey Kazarinov <kazarinov092@gmail.com>
2022-07-16 11:25:35 +0500

    feat(smoke): add smoke effect for title

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
+/**
+ * @const header - блок header
+ */
 const header = document.querySelector('.header');
+/**
+ * @const leters - элементы span, буквы заголовка шапки
+ */
+const letters = document.querySelectorAll('.header__title-item');
 
+/**
+ * событие прокручивания страницы для преобразования блока header
+ */
 window.addEventListener('scroll', () => {
   header.classList.toggle('sticky', window.scrollY > 0);
 })
+
+/**
+ * Эффект дыма для букв заголовка шапки
+ */
+for(let i = 0; i<letters.length; i++) {
+  letters[i].addEventListener('mouseover', () => {
+    letters[i].classList.toggle('header__title-item_active')
+  })
+}
