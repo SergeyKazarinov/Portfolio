a5afed1f281112edc1945d06dcb227193e60b294
Sergey Kazarinov <kazarinov092@gmail.com>
2022-08-07 00:24:25 +0500

    feat(hover): add hover to navigation and social-link

--- utils/scroll.js
+++ utils/scroll.js
@@ -2,6 +2,5 @@ let progress = document.getElementById('progressBar');
 let totalHeight = document.body.scrollHeight - window.innerHeight;
 window.onscroll = function() {
   let progressHeight = (window.pageYOffset / totalHeight) * 100;
-  console.log(progressHeight);
   progress.style.height = progressHeight + "%";
 }
